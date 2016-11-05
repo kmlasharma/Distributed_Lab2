bef36701cd828459ca9223299512f293e51cabb7
Kmla Sharma <KmlaSharma@kmlas-air.home>
2016-11-05 13:18:07 +0000

    Implemented threading and new shell script

---
+++ client.sh
@@ -0,0 +1,14 @@
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
+echo -n "HELO text\n" | nc localhost 8000
--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-import _thread
+
 import time
 import socket
 import threading
 
+MINIMAL_AMOUNT = 0
+MAX_AMOUNT = 3
 PORT = 8000
 HOST = "localhost"
 BUFFER = 1024
 UTF = "utf-8"
 RESPONSE_PACKET_ONE = 1
 STUDENT_ID = "13319349"
-pool = []
+allThreadsWorking = []
+waiting_conns = []
 
 def print_add(clientSocket, address):
 	print ("Received connection!")
@@ -18,9 +21,10 @@ def print_add(clientSocket, address):
 	if whichPacket == RESPONSE_PACKET_ONE:
 		response = "HELO text\nIP:[%s]\nPort:[%d]\nStudentID:[%s]\n" % (HOST, PORT, STUDENT_ID)
 		clientSocket.sendall(response.encode())
-
 	clientSocket.close()
 	print ("Closed connection!")
+	
+
 
 def handleInput(data):
 	if ("HELO text" in data):
@@ -31,15 +35,33 @@ def handleInput(data):
 def main():
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serversocket.bind((HOST, PORT))
-	while True:
-		serversocket.listen(5)
+	serversocket.listen(5)
+
+	while 1:
 		print ("Waiting for client connections...")
 		#accept connections from outside
+		print ("Current amount of working threads: %d" % (len(allThreadsWorking)))
+		print ("Current amount of queued connections: %d" % (len(waiting_conns)))
 		(clientsocket, address) = serversocket.accept()
-		thread = threading.Thread(target=print_add, args = (clientsocket, address))
-		# client socket = <socket.socket fd=6, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 8000), raddr=('127.0.0.1', 58745)>
-		# address = ('127.0.0.1', 58745)
-		thread.start()
+		waiting_conns.append((clientsocket, address))
+
+		
+
+		if (len(allThreadsWorking) < MAX_AMOUNT): #can create a new thread
+			connTuple = waiting_conns.pop()
+			clsocket = connTuple[0]
+			address = connTuple[1]
+
+			thread = threading.Thread(target=print_add, args = (clsocket, address))
+			allThreadsWorking.append(thread)
+			print ("Current amount of working threads: %d" % (len(allThreadsWorking)))
+			print ("Current amount of queued connections: %d" % (len(waiting_conns)))
+			thread.start()
+			allThreadsWorking.remove(thread)
+			thread.join()
+			
+			print ("Current amount of working threads: %d" % (len(allThreadsWorking)))
+			print ("Current amount of queued connections: %d" % (len(waiting_conns)))
 		# ct = client_thread(clientsocket)
 		# ct.run()
 
